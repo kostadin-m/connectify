5463f6f7a13f1555764884f4edfcb1bf5ce34a2e
kostadin-m <dinka4aps@gmail.com>
2023-01-30 13:20:15 +0100

    search bar for users added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useAuthContext } from '../../hooks/useAuthContext'
 import { Link } from 'react-router-dom'
 //icons
 import LightMode from '../../assets/light_mode_icon.svg'
+import SearchIcon from '../../assets/search_icon.svg'
 
 //styles
 import './Navbar.css'
@@ -20,11 +21,15 @@ export default function Navbar() {
                     <Link to='/' >
                         <h2>mySocialMedia</h2>
                     </Link>
+                    <input className ='nav-input' type='text' placeholder='Search Users' />
+                    <img className='search-icon' src={SearchIcon} alt='search-icon' />
+                </li>
+                <li>
                 </li>
                 <li className='nav-item'>
                     <img className='light-mode' onClick={toggleTheme} src={LightMode} alt='light mode icon' />
                 </li>
-                {!user ? <UserNavbar theme={theme} /> : <GuestNavbar />}
+                {user ? <UserNavbar theme={theme} /> : <GuestNavbar />}
             </ul>
         </nav>
     )

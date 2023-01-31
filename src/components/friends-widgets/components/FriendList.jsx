b507b649a2ccbb8384dc8fabb9ee0d4a6f0654c8
kostadin-m <dinka4aps@gmail.com>
2023-01-31 14:25:20 +0200

    fixes to list-friends components

--- src/components/friends-widgets/components/FriendList.jsx
+++ src/components/friends-widgets/components/FriendList.jsx
@@ -1,87 +1,92 @@
+import { Link } from "react-router-dom";
 
+import Accept from "../../../assets/accept_request.svg";
+import Deny from "../../../assets/close_icon.svg";
+import Test from "../../../assets/test.jpg";
 
-import Accept from '../../../assets/accept_request.svg'
-import Deny from '../../../assets/close_icon.svg'
-import Test from '../../../assets/test.jpg'
-
-
-import styles from '../Friends.module.css'
+import styles from "../Friends.module.css";
 
 export default function FriendList({ theme }) {
-    return (
-        <div className={styles.listFriends}>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-                       <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-                       <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-            <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
-                       <div className={`${styles.friend} ${styles[theme]}`}>
-                <img className='profile-image' src={Test} alt='profile picture' />
-                <p className={styles.name}>Kostadin Atanasov Majerski</p>
-                <div className={`${styles.buttons} ${styles[theme]}`}>
-                    <img className={styles.button} src={Accept} alt='accept icon' />
-                    <img className={styles.button} src={Deny} alt='accept icon' />
-                </div>
-            </div>
+  return (
+    <div className={styles.listFriends}>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+          <Link onClick={() => console.log("redirected")} to="/" />
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img
+            className={styles.button}
+            onClick={() => console.log("clicked")}
+            src={Accept}
+            alt="accept icon"
+          />
+          <img className={styles.button} src={Deny} alt="accept icon" />
         </div>
-    )
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+      <div className={`${styles.friend} ${styles[theme]}`}>
+        <img className="profile-image" src={Test} alt="profile picture" />
+        <p className={styles.name}>Kostadin Atanasov Majerski</p>
+        <div className={`${styles.buttons} ${styles[theme]}`}>
+          <img className={styles.button} src={Accept} alt="accept icon" />
+          <img className={styles.button} src={Deny} alt="accept icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
